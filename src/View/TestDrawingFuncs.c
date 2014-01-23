6ef95a8f9486467527eb46456794628e2ec33f7b
Ryan Morrison <morrisor@uci.edu>
2014-01-23 14:28:06 -0800

    View-level 'main' for testing draw and rendering functions

---
+++ src/View/TestDrawingFuncs.c
@@ -0,0 +1,65 @@
+/******************************************************************************/
+/* TestDrawingFuncs.c: main function for chess game                           */
+/******************************************************************************/
+/* Author: Ryan M. Morison                                                    */
+/*                                                                            */
+/* History:                                                                   */
+/*          12/22/13 initial file creation; draw window with checkerboard     */
+/*          12/26/13 generalize basic SDL operations into functions @ chess.h */
+/*          01/??/14 split chess.h into display.c/h and render.c/h            */
+/******************************************************************************/
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include "render.h"
+#include "display.h"
+#include "constants.h"
+
+
+int main(int argc, char *argv[]){
+    
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    
+    if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
+        window = SDL_CreateWindow(TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                  SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        renderer = SDL_CreateRenderer(window, -1, 0);
+    }
+    else{
+        //error check here
+    }
+    if(TTF_Init() != 0){
+        //error check here
+    }
+    
+    int quit = 0;
+    
+    // clear screen
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+    
+    /* testing draw functions here; they are not yet linked with events handling/bools */
+    /* so only one draw can be run at a time right now */
+    drawMainMenu(renderer);
+    
+    /* drawChessboard(); */
+    
+    /* drawPieces(); */
+    
+    SDL_RenderPresent(renderer);
+    
+    /* game loop */
+    while(!quit){
+        quit = events();	
+    }
+    
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    
+    clean();
+    
+    return 0;
+}
