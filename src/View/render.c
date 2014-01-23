3fce62a017179f78210b53d4a09cfdc19afd4061
Ryan Morrison <morrisor@uci.edu>
2014-01-23 14:32:21 -0800

    src for SDL rendering/loading functions

---
+++ src/View/render.c
@@ -0,0 +1,80 @@
+/************************************************************************/
+/* render.c: source file for rendering/loading functions                */
+/************************************************************************/
+/* Author: Ryan M. Morrison                                             */
+/*                                                                      */
+/* History:                                                             */
+/*          12/26/13 initial file creation                              */
+/*          12/27/13 added text rendering function                      */
+/*          12/30/13 added texture loading and rendering functions      */
+/*          01/19/14 added second texture function to preserve surface  */
+/*                   dimensions                                         */
+/************************************************************************/
+
+#include <stdio.h>
+#include "render.h"
+
+/* function to load a texture */
+SDL_Texture *loadTexture(const char *fileName, SDL_Renderer *renderer){
+    
+  SDL_Texture *texture = NULL;
+  SDL_Surface *loadedImage = NULL;
+  
+  loadedImage = IMG_Load(fileName);
+
+  if(loadedImage){
+
+    texture = SDL_CreateTextureFromSurface(renderer, loadedImage);
+    SDL_FreeSurface(loadedImage);
+
+    if(!loadedImage){
+      //error
+    }
+  }
+  //SDL_Texture *texture = IMG_LoadTexture(renderer, fileName);
+   //error check
+   return texture;
+}
+
+/* function to render a texture with specified(scaled) size */
+void renderTexture(SDL_Texture *texture, SDL_Renderer *renderer, int x, int y, int w, int h){
+    
+    SDL_Rect dest;
+    dest.x = x;
+    dest.y = y;   
+    dest.w = w;
+    dest.h = h;
+    
+    SDL_RenderCopy(renderer, texture, NULL, &dest);
+}
+
+/* function to render a texture with native(no scaling) size */
+void renderTexture2(SDL_Texture *texture, SDL_Renderer *renderer, int x, int y){
+
+  int width, height;
+  SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+  renderTexture(texture, renderer, x, y, width, height);
+}
+
+/* function to render text */
+SDL_Texture *renderText(const char *message, const char *fontFile, SDL_Color color,
+			int fontSize, SDL_Renderer *renderer){
+
+  //open desired font
+  TTF_Font *font = TTF_OpenFont(fontFile, fontSize);
+
+  /* error check */
+
+  //render to a surface and load surface into a texture
+  SDL_Surface *surface = TTF_RenderText_Blended(font, message, color);
+
+  /* error check */
+
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+  /* error check */
+
+  SDL_FreeSurface(surface);
+  TTF_CloseFont(font);
+  return texture;
+}
