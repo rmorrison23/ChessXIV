ca40d999744f31039154be8b3dbf18020dafb84f
Ryan Morrison <morrisor@uci.edu>
2014-01-23 14:29:20 -0800

    View-level const. header for quick adjustments to graphics attributes

---
+++ src/View/constants.h
@@ -0,0 +1,29 @@
+/************************************************************/
+/* constants.h: header file for chess game constants        */
+/************************************************************/
+/* Author: Ryan M. Morrison                                 */
+/*                                                          */
+/* History:                                                 */
+/*          12/26/13 initial file creation                  */
+/************************************************************/
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+static const int SCREEN_WIDTH  = 1200;
+static const int SCREEN_HEIGHT = 675;
+static const int BOARD_WIDTH   = 600;
+static const int BOARD_HEIGHT  = 600;
+static const int BUTTON_WIDTH  = 200;
+static const int BUTTON_HEIGHT = 75;
+static const int TITLE_WIDTH   = 1100;
+static const int TITLE_HEIGHT  = 200;
+
+static const char *TITLE       = "C h e s s  X I V";
+static const char *ONE_PLAYER  = "One Player";
+static const char *TWO_PLAYERS = "Two Players";
+static const char *ADVANCED    = "Advanced";
+
+static const char *CALIBRI_FONT = "Assets/fonts/Calibri.ttf";
+
+#endif
