6c4c6a741671658337b74ab818737bac268d4580
mqchau <mqchau@uci.edu>
2014-01-26 20:49:00 -0800

    Writing test for Coordinate List

--- src/Model/ModelUnitTest.c
+++ src/Model/ModelUnitTest.c
@@ -1,6 +1,12 @@
 #include "Model.h"
 
+#define TEST_COORDINATE_LIST  
+
 int main(void){
-  ModelSpeak();
+#ifdef TEST_COORDINATE_LIST  
+	ChessCoordinateList * MainList = malloc(sizeof(ChesscoordinateList));
+	
+	ChessCoordinateList_Free(MainList);
+#endif
   return 0;
 }
