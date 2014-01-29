23be2d74bcf411323cfa620c9cba9d22b21d9c87
Quan Chau <quanc@broadcom.com>
2014-01-29 10:37:05 -0800

    Update ChessBoard_InitializeEmpty

--- src/Model/ChessBoard.c
+++ src/Model/ChessBoard.c
@@ -191,168 +191,23 @@ ChessBoard * ChessBoard_Initialize(void){
 ChessBoard * ChessBoard_InitializeEmpty(void){
 	
 	/*malloc the board*/
-	ChessBoard * ChessBoardToReturn = (ChessBoard *) malloc(sizeof(ChessBoard));
-	assert(ChessBoardToReturn);
-  
-	/*malloc two players*/
-	ChessBoardToReturn->WhitePlayer = (ChessPlayer *) malloc(sizeof(ChessPlayer));
-	ChessBoardToReturn->BlackPlayer = (ChessPlayer *) malloc(sizeof(ChessPlayer));
-	assert(ChessBoardToReturn->WhitePlayer && ChessBoardToReturn->BlackPlayer);
-	ChessBoardToReturn->WhitePlayer->PlayerColor = White;
-	ChessBoardToReturn->BlackPlayer->PlayerColor = Black;
-	ChessBoardToReturn->WhitePlayer->OtherPlayer = ChessBoardToReturn->BlackPlayer;
-	ChessBoardToReturn->BlackPlayer->OtherPlayer = ChessBoardToReturn->WhitePlayer;
+	ChessBoard * ChessBoardToReturn =  ChessBoard_Initialize();
 	
-	/*malloc all coordinate on the board*/
+	/*break the link between pieces and coordinates*/
 	int i,j;
-	for (i = 0; i < CHESS_BOARD_MAX_ROW; i++){
-		for (j = 0; j < CHESS_BOARD_MAX_COL; j++){
-			ChessBoardToReturn->Board[i][j]	= (ChessCoordinate *) malloc(sizeof(ChessCoordinate));
-			assert(ChessBoardToReturn->Board[i][j]);
-			ChessBoardToReturn->Board[i][j]->Piece = NULL;
-			ChessBoardToReturn->Board[i][j]->Rank = i;
-			ChessBoardToReturn->Board[i][j]->File = j;
-			ChessBoardToReturn->Board[i][j]->MainBoard = ChessBoardToReturn;
-			
-		}
+	for (i = 0; i < 16; i++){
+		ChessBoardToReturn->BlackPlayer->Pieces[i]->Coordinate = NULL;
+		ChessBoardToReturn->BlackPlayer->Pieces[i]->AliveFlag = False;
+		ChessBoardToReturn->WhitePlayer->Pieces[i]->Coordinate = NULL;
+		ChessBoardToReturn->WhitePlayer->Pieces[i]->AliveFlag = False;
 	}
 	
-	/*malloc each piece and assign all double links*/
-	/*White first*/
-	/*Pawns x 8*/
-	ChessPiece * CurrPiece;
-	int CurrPieceIdx = 0;
-	for (i = 0; i < 8; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Pawn;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->WhitePlayer;		
-		CurrPiece->AliveFlag = False;
-		CurrPiece->PawnMoveFirstFlag = True;
-		ChessBoardToReturn->WhitePlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+		ChessBoardToReturn->Board[i][j]->Piece = NULL;
 	
-	/*Rook x 2*/
-	for (i = 0; i < 2; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Rook;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->WhitePlayer;		
-		CurrPiece->AliveFlag = False;
-		ChessBoardToReturn->WhitePlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
+	return ChessBoardToReturn;
 	
-	/*Knight x 2*/
-	for (i = 0; i < 2; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Knight;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->WhitePlayer;		
-		CurrPiece->AliveFlag = False;
-		ChessBoardToReturn->WhitePlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
-	
-	/*Bishop x 2*/
-	for (i = 0; i < 2; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Bishop;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->WhitePlayer;		
-		CurrPiece->AliveFlag = False;
-		ChessBoardToReturn->WhitePlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
-	
-	/*Queen x 1*/
-	CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-	assert(CurrPiece);
-	CurrPiece->Type = Queen;
-	CurrPiece->Index = 0;
-	CurrPiece->Player = ChessBoardToReturn->WhitePlayer;	
-	CurrPiece->AliveFlag = False;
-	ChessBoardToReturn->WhitePlayer->Pieces[CurrPieceIdx++] = CurrPiece;
-	
-	
-	/*King x 1*/
-	CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-	assert(CurrPiece);
-	CurrPiece->Type = King;
-	CurrPiece->Index = 0;
-	CurrPiece->Player = ChessBoardToReturn->WhitePlayer;	
-	CurrPiece->AliveFlag = False;
-	ChessBoardToReturn->WhitePlayer->Pieces[CurrPieceIdx++] = CurrPiece;	
-	
-	
-	/*Then black*/
-	/*Pawns x 8*/
-	CurrPieceIdx = 0;
-	for (i = 0; i < 8; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Pawn;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->BlackPlayer;		
-		CurrPiece->AliveFlag = False;
-		CurrPiece->PawnMoveFirstFlag = True;
-		ChessBoardToReturn->BlackPlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
-	
-	/*Rook x 2*/
-	for (i = 0; i < 2; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Rook;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->BlackPlayer;		
-		CurrPiece->AliveFlag = False;
-		ChessBoardToReturn->BlackPlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
-	
-	/*Knight x 2*/
-	for (i = 0; i < 2; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Knight;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->BlackPlayer;		
-		CurrPiece->AliveFlag = False;
-		ChessBoardToReturn->BlackPlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
-	
-	/*Bishop x 2*/
-	for (i = 0; i < 2; i++){
-		CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-		assert(CurrPiece);
-		CurrPiece->Type = Bishop;
-		CurrPiece->Index = i;
-		CurrPiece->Player = ChessBoardToReturn->BlackPlayer;		
-		CurrPiece->AliveFlag = False;
-		ChessBoardToReturn->BlackPlayer->Pieces[CurrPieceIdx++] = CurrPiece;		
-	}
-	
-	/*Queen x 1*/
-	CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-	assert(CurrPiece);
-	CurrPiece->Type = Queen;
-	CurrPiece->Index = 0;
-	CurrPiece->Player = ChessBoardToReturn->BlackPlayer;	
-	CurrPiece->AliveFlag = False;
-	ChessBoardToReturn->BlackPlayer->Pieces[CurrPieceIdx++] = CurrPiece;
-	
-	
-	/*King x 1*/
-	CurrPiece = (ChessPiece *) malloc(sizeof(ChessPiece));
-	assert(CurrPiece);
-	CurrPiece->Type = King;
-	CurrPiece->Index = 0;
-	CurrPiece->Player = ChessBoardToReturn->BlackPlayer;	
-	CurrPiece->AliveFlag = False;
-	ChessBoardToReturn->BlackPlayer->Pieces[CurrPieceIdx++] = CurrPiece;
-	
-  return ChessBoardToReturn;
 }
 
 void ChessBoard_Free(ChessBoard * CurrBoard){
---
+++ src/Model/TestSpecialMove_EnPassant.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Model.h"
+#include "View.h"
+
+int main(void){
+	char temp_char;
+	
+	/*initialize chess board emtpy*/
+	ChessBoard * MainChessBoard = ChessBoard_InitializeEmpty();
+	/*move list*/
+	ChessMoveList * LocalMoveList = ChessMoveList_Initialize();
+	
+	/*set up king and some pawns of white, we'll test on them*/
+	ChessPiece * WKing = ChessPlayer_GetChessPiece(MainChessBoard->WhitePlayer, King, 0);
+	WKing->AliveFlag = True;
+	WKing->Coordinate = MainChessBoard->Board[0][4];
+	MainChessBoard->Board[0][4]->Piece = WKing;
+	ChessPiece * WRook = ChessPlayer_GetChessPiece(MainChessBoard->WhitePlayer, Rook, 0);
+	WRook->AliveFlag = True;
+	WRook->Coordinate = MainChessBoard->Board[0][0];
+	MainChessBoard->Board[0][0]->Piece = WRook;	
+	
+	/*set up some other piece of whites*/
+		
+	/*set up some other pieces of black*/
+	ChessPiece * BKing = ChessPlayer_GetChessPiece(MainChessBoard->BlackPlayer, King, 0);
+	BKing->AliveFlag = True;
+	BKing->Coordinate = MainChessBoard->Board[7][4];
+	MainChessBoard->Board[7][4]->Piece = BKing;
+	
+	ChessPiece * BRook = ChessPlayer_GetChessPiece(MainChessBoard->BlackPlayer, Rook, 0);
+	BRook->AliveFlag = True;
+	BRook->Coordinate = MainChessBoard->Board[4][3];
+	MainChessBoard->Board[4][3]->Piece = BRook;
+	/*display the board*/
+	DisplayChessBoard(MainChessBoard);
+	scanf("%c", &temp_char);
+	
+	/*do the test twice*/
+	int i = 0;
+	ChessCoordinateList * LegalCoordList;
+	ChessCoordinate * NextPos;
+	ChessMove * CurrMove;
+	for (i = 0; i < 2; i++){
+		/*get legal coordinate by the W King, it should shows positions it can move to*/
+		/*conditions must be followed to allow castling (from wiki)*/
+		
+		/* The king and the chosen rook are on the player's first rank.[3]
+		Neither the king nor the chosen rook have previously moved.
+		There are no pieces between the king and the chosen rook.
+		The king is not currently in check.
+		The king does not pass through a square that is attacked by an enemy piece.[4]
+		The king does not end up in check. (True of any legal move.)*/
+		
+		LegalCoordList = Model_GetLegalCoordinates(MainChessBoard, WKing, MainChessBoard->WhitePlayer);		
+		
+		
+		/*highlight the board*/
+		HighlightCoordinates(MainChessBoard, LegalCoordList);		
+		ChessCoordinateList_Free(LegalCoordList);
+				
+		/*get coordinate selection from user*/
+		NextPos = View_GetOneCoordinate(MainChessBoard);
+		
+		/*construct a move*/
+		CurrMove = ChessMove_Initialize();
+		CurrMove->MovePiece = WKing;
+		CurrMove->StartPosition = WKing->Coordinate;
+		CurrMove->NextPosition = NextPos;
+		
+		/*perform move*/
+		Model_PerformMove(MainChessBoard, LocalMoveList, CurrMove);
+		
+		/*display the board*/
+		DisplayChessBoard(MainChessBoard);
+		scanf("%c", &temp_char);
+		
+		/*do a random move from black*/
+		CurrMove = ChessMove_Initialize();
+		CurrMove->MovePiece = BKing;
+		CurrMove->StartPosition = BKing->Coordinate;
+		CurrMove->NextPosition = MainChessBoard->Board[6][4];
+		Model_PerformMove(MainChessBoard, LocalMoveList, CurrMove);
+		
+		/*display the board*/
+		DisplayChessBoard(MainChessBoard);
+		scanf("%c", &temp_char);
+		
+		/*undo the move, suppose to reverse both black and and white move*/
+		Model_UndoLastMove(MainChessBoard, LocalMoveList);
+		
+		/*display the board*/
+		DisplayChessBoard(MainChessBoard);
+		scanf("%c", &temp_char);
+	}
+	/*free the board*/
+	ChessBoard_Free(MainChessBoard);
+	
+	/*free move list*/
+	ChessMoveList_Free(LocalMoveList);
+	
+	return 0;
+}
