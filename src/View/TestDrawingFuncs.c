2fc44969ca8d246939f62251f40a8220dfa8cc34
Ryan Morrison <morrisor@uci.edu>
2014-02-01 16:48:38 -0800

    testing 4 fundamental functions
    image, text, filled box, and outline box - all working

--- src/View/TestDrawingFuncs.c
+++ src/View/TestDrawingFuncs.c
@@ -34,36 +34,59 @@ int main(int argc, char *argv[]){
     }
         
     // clear screen
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
     SDL_RenderClear(renderer);
+    /* ************************************testing************************************************ */
     
-    int zeroCase = 0;
-    int *screenMode;     /* 0 = main, 1 = one player, 2 = two players, 3 = game screen, 100 = esc */
-    screenMode = &zeroCase;
-    int playing = 0;
-    int on = 1;
+    /* IMAGE: This one works */
+    SDL_Texture * backSplash = loadTexture("Assets/Menu_Backgrounds/Background_1600_900.jpg", renderer);
+    renderTexture(backSplash, renderer, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    /* SDL_RenderPresent(renderer); */
 
-    *screenMode = drawMainMenu(window, renderer, screenMode);
+    /* TEXT: This one works */
+    SDL_Color testColor = {255, 255, 255};
+    SDL_Texture *testText = renderText("This is a test!!!", "Assets/fonts/Calibri.ttf", testColor, 70, renderer);
+    renderTexture2(testText, renderer, 50, 50);
+    /* SDL_RenderPresent(renderer); */
+
+    /* FILLED BOX: This one works */
+    SDL_Texture *testBox = renderFilledBox(255, 255, 255, 255, 50, 200, 75, 75, renderer);
+    renderTexture2(testBox, renderer, NULL, NULL);
+    /* SDL_RenderPresent(renderer); */
+
+    /* EMPTY BOX (OUTLINE): This one works */
+    SDL_Texture *testOutlineBox = renderEmptyBox(255, 255, 255, 255, 150, 200, 75, 75, renderer);
+    renderTexture2(testOutlineBox, renderer, NULL, NULL);
+    SDL_RenderPresent(renderer);
+
+    /* **********************************OLD STUFF****************************************************** */
+    /* int zeroCase = 0; */
+    /* int *screenMode;     /\* 0 = main, 1 = one player, 2 = two players, 3 = game screen, 100 = esc *\/ */
+    /* screenMode = &zeroCase; */
+    /* int playing = 0; */
+    /* int on = 1; */
+
+    /* *screenMode = drawMainMenu(window, renderer, screenMode); */
   
-      while(!playing){
+    /*   while(!playing){ */
 
-	if(*screenMode == 0){
-	  *screenMode = drawMainMenu(window, renderer, screenMode);
-	}
-	if(*screenMode == 1){
-	  *screenMode = drawOnePlayerMenu(window, renderer, screenMode);
-	}
-	if(*screenMode == 2){
-	  *screenMode = drawTwoPlayerMenu(window, renderer, screenMode);
-	}
-	if(*screenMode == 3){
-	  drawGameplayScreen(window, renderer);
-	  playing = 1;
-	}
-	if(*screenMode == 100){
-	  break;
-	}
-      }    
+    /* 	if(*screenMode == 0){ */
+    /* 	  *screenMode = drawMainMenu(window, renderer, screenMode); */
+    /* 	} */
+    /* 	if(*screenMode == 1){ */
+    /* 	  *screenMode = drawOnePlayerMenu(window, renderer, screenMode); */
+    /* 	} */
+    /* 	if(*screenMode == 2){ */
+    /* 	  *screenMode = drawTwoPlayerMenu(window, renderer, screenMode); */
+    /* 	} */
+    /* 	if(*screenMode == 3){ */
+    /* 	  drawGameplayScreen(window, renderer); */
+    /* 	  playing = 1; */
+    /* 	} */
+    /* 	if(*screenMode == 100){ */
+    /* 	  break; */
+    /* 	} */
+    /*   }     */
    
 
     /* drawOnePlayerMenu(window, renderer); */
@@ -82,7 +105,7 @@ int main(int argc, char *argv[]){
 
     eventMain();
 
-    SDL_RenderPresent(renderer);
+    /* SDL_RenderPresent(renderer); */
    
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
